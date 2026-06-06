7b04e81bc6ba00252dbaf3458ceeea8b2d50b322
maximusdionyssopoulos <max@sagecool.com>
2026-06-07 09:40:35 +1000

    add keymaps for copying file paths

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -9,3 +9,7 @@ vim.keymap.set("n", "<leader>E", "<CMD>Oil.<CR>", { desc = "Open current working
 -- FFF
 vim.keymap.set('n', '<leader><leader>', function() require('fff').find_files() end, { desc = 'FFFind files' })
 vim.keymap.set('n', '<leader>/', function() require("fff").live_grep() end, {desc = "FFFind content (Grep)"})
+
+--- Helpers
+vim.keymap.set("n", '<leader>y', function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, {desc = "Copy absolute path"})
+vim.keymap.set("n", '<leader>yy', function() vim.fn.setreg("+", vim.fn.expand("%:.")) end, {desc = "Copy relative path"})
