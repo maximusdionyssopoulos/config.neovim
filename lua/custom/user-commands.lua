a0b322dcbfc9bcd270c67955721fb960865cd13a
maximusdionyssopoulos <max@sagecool.com>
2026-06-08 21:33:35 +1000

    add config plugin audit command

--- lua/custom/user-commands.lua
+++ lua/custom/user-commands.lua
@@ -2,3 +2,6 @@ local crt_u_cmd = vim.api.nvim_create_user_command
 
 crt_u_cmd("CopyAbsolutePath", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, {})
 crt_u_cmd("CopyRelativePath", function() vim.fn.setreg("+", vim.fn.expand("%:.")) end, {})
+
+-- not active ; gra to open delete menu
+crt_u_cmd("ConfigAuditPlugins", function() vim.pack.update(nil, { offline = true }) end, {})
