4db1f17bf67a7469e01fab546587973f6c4fd35d
maximusdionyssopoulos <max@sagecool.com>
2026-06-08 12:49:51 +1000

    add lua autocmd for reacting to the sigusr1 event (zsh script sends) to switch theme

---
+++ lua/custom/set-theme.lua
@@ -0,0 +1,21 @@
+vim.api.nvim_create_autocmd("Signal", {
+	pattern = "SIGUSR1",
+	callback = function ()
+local file = io.open(vim.fn.expand("~/.config/theme/state"), "r")
+  if not file then
+    vim.notify("Could not read theme file:", vim.log.levels.WARN)
+    return
+  end
+
+  local mode = file:read("*a")
+  file:close()
+
+  mode = mode:gsub("%s+", "")
+  if not mode == "dark" or not mode == "light" then
+    vim.notify("Invalid theme value: " .. mode, vim.log.levels.WARN)
+    return
+  end
+vim.o.background = mode
+vim.cmd('redraw')
+end
+})
