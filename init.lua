8c6ba236aec60f10bee77a383d3f356bb114391b
maximusdionyssopoulos <max@sagecool.com>
2026-06-08 12:49:03 +1000

    update custom init

--- init.lua
+++ init.lua
@@ -1,10 +1,5 @@
 require("plugins")
-require("lsp")
-require("custom.re+keymap")
-require("custom.ui")
-
--- Always use system clipboard
-vim.opt.clipboard:append("unnamedplus")
+require("custom")
 
 -- Inline diagnostic
 vim.diagnostic.config({virtual_text = true})
---
+++ lua/custom/init.lua
@@ -0,0 +1,4 @@
+require("custom.user-commands")
+require("custom.re+keymap")
+require("custom.ui")
+require("custom.set-theme")
