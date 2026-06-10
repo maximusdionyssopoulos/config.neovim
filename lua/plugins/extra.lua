519d097fd8bd9450ec853dc16aa3db602e55545a
maximusdionyssopoulos <max@sagecool.com>
2026-06-10 22:06:44 +1000

    change || pair mapping to ruby only

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -1,8 +1,18 @@
 require("nvim-surround").setup()
-require("mini.pairs").setup({
-  mappings = {
-    ["|"] = { action = "closeopen", pair = "||", neigh_pattern = "^[^\\]", register = { cr = false } },
-  },
+
+require("mini.pairs").setup()
+-- Only add the || pair for ruby files
+-- ruby uses |...| for blocks but it also uses ||
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  callback = function()
+    MiniPairs.map_buf(
+      0,
+      "i",
+      "|",
+      { action = "closeopen", pair = "||", neigh_pattern = "^[^\\]", register = { cr = false } }
+    )
+  end,
 })
 
 require("mini.indentscope").setup({
